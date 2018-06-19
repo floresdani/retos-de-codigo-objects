b7ef7a2c78963ab13abe010ced9ffe545340e94a
floresdani <florescaceresdaniela@gmail.com>
2018-06-19 11:49:33 -0400

    subiendo ejercicio 1 completado, pendientes 2 y 3

---
+++ reto1.js
@@ -0,0 +1,20 @@
+/*Escribe una función llamada "addArrayProperty(obj, key, array)".
+
+Dado un objeto, una clave y un array, "addArrayProperty" establece una nueva propiedad sobre el objeto en la clave dada, con su valor establecido en el array dado.
+
+Ejemplo de entrada:
+
+var myObj = {};
+var myStr = 'myProperty';
+var myArray = [1, 3];
+
+addArrayProperty(myObj, myStr, myArray);
+console.log(myObj.myProperty); // --> [1, 3] */
+
+
+function addArrayProperty(obj, key, arr) {
+  obj[key] = arr;
+  return arr;
+}
+
+module.exports = addArrayProperty;
